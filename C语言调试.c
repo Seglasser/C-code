8955acff624b9461f9bbe67852cb0e2885fd2dc0
Seglasser <88964680+Seglasser@users.noreply.github.com>
2021-08-16 17:48:57 +0800

    VS调试（1）

---
+++ C语言调试.c
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%d ",i);
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	for (i = 0; i <= 12; i++)
+//	{
+//		printf("hehe\n");
+//		//arr [i] = 0;
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%d ", i);
+//	}
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%d ", 10-i);
+//	}
+//	return 0;
+//}
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int main()
+//{
+//	printf("hehe\n");
+//	int a = 10;
+//	int  b = 20;
+//	int c = Add(a, b);
+//	return 0;
+//}
+
