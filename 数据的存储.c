143f69a38f42b4481e6676a7bbe2900d19111c6d
Seglasser <88964680+Seglasser@users.noreply.github.com>
2021-08-21 22:10:36 +0800

    （C语言进阶）数据的存储

---
+++ 数据的存储.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	int a = 10;
+//	float b = 10.0;
+//	short c = 10;
+//	return 0;
+//}
+
+//void test()
+//{
+//	printf("hehe\n");
+//}
+//int main()
+//{
+//	test(100);
+//	return 0;
+//}
+
+int main()
+{
+	int a = 10;
+	int b = -10;
+	return 0;
+}
