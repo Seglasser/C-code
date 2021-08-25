4fca2b2a5c77e81c8cf4bfc84805378b9c877834
Seglasser <88964680+Seglasser@users.noreply.github.com>
2021-08-25 16:45:44 +0800

    数据存储（3）

---
+++ 数据存储（3）.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	int i = 10;
+//	unsigned int j = 10;
+//	printf("%d\n",i+j);//按照补码形式计算，最终结果格式化为有符号整数
+//	return 0;
+//}
+
+//int main()
+//{
+//	char a[1000];
+//	int i;
+//	for (i = 0; i < 1000; i++)
+//	{
+//		a[i] = -1 - i;
+//	}
+//	printf("%d\n",strlen(a));//算到“\0”，即字符数组char a[1000]中存放的数字是ASCII码表中所对应的字符，在ASCII码表中的0对应“\0”
+//	return 0;
+//}
+
+//unsigned char i = 0;
+//int  main()//最终死循环
+//{
+//	for (i = 0; i <= 255; i++)//在无符号数unsigned char中 0——255，255+1后返回0
+//	{
+//		printf("Hello World\n");
+//	}
+//	return 0;
+//}
