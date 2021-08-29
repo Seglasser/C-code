a654340185637834cb39b2032e90de2fa92feb20
Seglasser <88964680+Seglasser@users.noreply.github.com>
2021-08-29 17:54:26 +0800

    数据存储(4)

---
+++ 数据存储（4）.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	int n = 9;
+//	float* pFloat = (float*)&n;
+//	printf("n的值为：%d\n", n);
+//	printf("*pFloat的值为：%f\n", *pFloat);
+//	*pFloat = 9.0;
+//	printf("num的值为：%d\n", n);
+//	printf("*pFloat的值为：%f\n", *pFloat);
+//	return 0;
+//}
+
+int main()
+{
+	float f = 5.5;
+	return 0;
+}
